91dd223bee2054bdefab6527605f1e654b40265a
David Granström <david@davidgranstrom.com>
2020-04-17 11:34:26 +0200

    Add Tabline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -25,7 +25,7 @@ local function active_right()
   -- filetype
   stl = stl .. '%y '
   -- line num, virtual col, value of char (hex)
-  stl = stl .. '%-8.(%l:%v %B%) '
+  stl = stl .. '%-10.(%l:%v %B%) '
   -- percentage through file
   stl = stl .. '%P'
   -- display scsynth status
@@ -43,12 +43,47 @@ local function inactive_right()
   return ''
 end
 
+--- Tabline
+
+local function get_short_file_name(handle)
+  local win = vim.api.nvim_tabpage_get_win(handle)
+  local buf = vim.api.nvim_win_get_buf(win)
+  local name = vim.api.nvim_buf_get_name(buf)
+  name = vim.call('fnamemodify', name, ':p:t')
+  return name == '' and 'No name' or name
+end
+
+local function create_tabline()
+  local tabline = ''
+  local tabs = vim.api.nvim_list_tabpages()
+  local num_tabs = vim.call('tabpagenr', '$')
+  for i, handle in ipairs(tabs) do
+    local n = vim.call('tabpagenr')
+    local name = get_short_file_name(handle)
+    if i == n then
+      tabline = tabline .. '%#TabLineSel#' .. name
+    else
+      tabline = tabline .. '%#TabLine#' .. name
+    end
+    if i ~= num_tabs then
+      tabline = tabline .. ' | '
+    end
+  end
+  return tabline
+end
+
+--- Interface
+
 function M.active()
   return active_left() .. '=' .. active_right()
 end
 
 function M.inactive()
   return inactive_left() .. '=' .. inactive_right()
+end
+
+function M.my_tabline()
+  return create_tabline()
 end
 
 return M
--- plugin/statusline.vim
+++ plugin/statusline.vim
@@ -4,12 +4,6 @@
 
 scriptencoding utf-8
 
-augroup statusline
-  autocmd!
-  autocmd WinEnter,BufEnter * setlocal statusline=%!ActiveStatus()
-  autocmd WinLeave,BufLeave * setlocal statusline=%!InactiveStatus()
-augroup END
-
 function! ActiveStatus() abort
   return luaeval('require("statusline").active()')
 endfunction
@@ -17,3 +11,15 @@ endfunction
 function! InactiveStatus() abort
   return luaeval('require("statusline").inactive()')
 endfunction
+
+function! Tabline() abort
+  return luaeval('require("statusline").my_tabline()')
+endfunction
+
+augroup statusline
+  autocmd!
+  autocmd WinEnter,BufEnter * setlocal statusline=%!ActiveStatus()
+  autocmd WinLeave,BufLeave * setlocal statusline=%!InactiveStatus()
+augroup END
+
+set tabline=%!Tabline()
