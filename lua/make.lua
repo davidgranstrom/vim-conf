0da779107352cd08832207e75cbc25952046a75f
David Granström <david@davidgranstrom.com>
2020-05-04 21:34:59 +0200

    Display failed tests if any

--- lua/make.lua
+++ lua/make.lua
@@ -5,6 +5,7 @@
 
 local M = {}
 local uv = vim.loop
+local stdout_log = ''
 
 --- Utils
 
@@ -26,6 +27,52 @@ local function check_dir_exists(dir)
     return stat.type == 'directory'
   end
   return false
+end
+
+local function parse_build_progress(data)
+  local progress, state = data:match('(%d+%%)%] (%a+)')
+  if progress and state then
+    vim.schedule_wrap(function()
+      local build = string.format('%s %s', state, progress)
+      vim.api.nvim_win_set_var(0, 'make_progress', build)
+    end)()
+  end
+end
+
+local function parse_ctest(data)
+  local ctest_num = data:match('^%s*(%d+/%d+)')
+  if ctest_num then
+    local status = data:match('Passed') and 'Passed' or 'Failed'
+    vim.schedule_wrap(function()
+      local ctest = string.format('%s %s', ctest_num, status)
+      vim.api.nvim_win_set_var(0, 'make_progress', ctest)
+    end)()
+  end
+end
+
+local function parse_ctest_fails(data)
+  local index = data:find('The following tests FAILED:')
+  if index then
+    return data:sub(index)
+  end
+end
+
+local function on_handle_exit(code)
+  local msg = ''
+  if code == 0 then
+    msg = 'Build OK'
+  else
+    msg = 'Build ERR'
+    local ctest_fails = parse_ctest_fails(stdout_log)
+    if ctest_fails then
+      vim.schedule_wrap(function()
+        vim.call('MakeQuickFix', ctest_fails)
+      end)()
+    end
+  end
+  vim.schedule_wrap(function()
+    vim.api.nvim_win_set_var(0, 'make_progress', msg)
+  end)()
 end
 
 local function run_make(opts)
@@ -54,6 +101,8 @@ local function run_make(opts)
   return uv.spawn('make', options, opts.on_exit)
 end
 
+--- Handlers
+
 local on_stderr = function(fd) 
   local s = ''
   uv.read_start(fd, function(err, data)
@@ -69,33 +118,14 @@ local on_stderr = function(fd)
   end)
 end
 
-local function parse_build_progress(data)
-  local progress, state = data:match('(%d+%%)%] (%a+)')
-  if progress and state then
-    vim.schedule_wrap(function()
-      local build = string.format('%s %s', state, progress)
-      vim.api.nvim_win_set_var(0, 'make_progress', build)
-    end)()
-  end
-end
-
-local function parse_ctest(data)
-  local ctest_num = data:match('^%s*(%d+/%d+)')
-  if ctest_num then
-    local status = data:match('Passed') and 'Passed' or 'Failed'
-    vim.schedule_wrap(function()
-      local ctest = string.format('%s %s', ctest_num, status)
-      vim.api.nvim_win_set_var(0, 'make_progress', ctest)
-    end)()
-  end
-end
-
 local on_stdout = function(fd) 
   local s = ''
   uv.read_start(fd, function(err, data)
     assert(not err, err)
     if data then
       s = s .. data
+      -- save complete log
+      stdout_log = stdout_log .. s
       local lines = vim.gsplit(s, '\n')
       for line in lines do
         if line ~= '' then
@@ -109,20 +139,23 @@ local on_stdout = function(fd)
   end)
 end
 
+--- Interface
+
 local handle, pid;
 
 function M.make()
   local stdout = uv.new_pipe(false)
   local stderr = uv.new_pipe(false)
+
   local on_exit = function(code, signal)
     stdout:close()
     stderr:close()
     handle:close()
-    local msg = code == 0 and 'Build OK' or 'Build ERR'
-    vim.schedule_wrap(function()
-      vim.api.nvim_win_set_var(0, 'make_progress', msg)
-    end)()
+    on_handle_exit(code)
   end
+
+  -- clear log
+  stdout_log = ''
 
   handle, pid = run_make({
     stdout = stdout,
