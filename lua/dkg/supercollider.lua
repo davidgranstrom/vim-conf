85bffceae9e1594e631779a115d30003d6652dc5
David Granström <david@davidgranstrom.com>
2020-11-17 19:14:07 +0100

    Add border around post window

--- lua/dkg/supercollider.lua
+++ lua/dkg/supercollider.lua
@@ -11,44 +11,107 @@ api.nvim_command([[
   autocmd FileType supercollider lua require'dkg/supercollider'.set_mappings()
 ]])
 
-function M.is_valid()
-  return M.bufnr and api.nvim_buf_is_loaded(M.bufnr) or false
+local Window = {}
+
+function Window:new(tbl)
+  tbl = tbl or {}
+  setmetatable(tbl, self)
+  self.__index = self
+  self.bufnr = api.nvim_create_buf(true, true)
+  self.border_bufnr = api.nvim_create_buf(false, true)
+  api.nvim_buf_set_option(self.bufnr, 'filetype', 'scnvim')
+  api.nvim_buf_set_name(self.bufnr, '[sclang]')
+  return tbl
 end
 
-function M.clear()
-  if M.is_valid() then
-    api.nvim_buf_set_lines(M.bufnr, 0, -1, true, {})
+function Window:is_open()
+  return self:is_valid() and vim.fn.bufwinnr(self.bufnr) > 0
+end
+
+function Window:is_valid()
+  return self.bufnr and api.nvim_buf_is_loaded(self.bufnr) or false
+end
+
+function Window:set_lines(data)
+  if self:is_valid() then
+    api.nvim_buf_set_lines(self.bufnr, -1, -1, true, {data})
+    if self:is_open() then
+      local num_lines = api.nvim_buf_line_count(self.bufnr)
+      api.nvim_win_set_cursor(self.winnr, {num_lines, 0})
+    end
   end
 end
 
-function M.is_open()
-  return M.is_valid() and vim.fn.bufwinnr(M.bufnr) > 0
+function Window:create_border(options, margin)
+  vim.validate {
+    options = {options, 'table'}
+  }
+  margin = margin or 2
+  local opts = {
+    width = options.width + margin,
+    height = options.height + margin,
+    col = options.col - (margin / 2),
+    row = options.row - (margin / 2),
+  }
+  local border = {}
+  local top = '╭' .. string.rep('─', opts.width - margin) .. '╮'
+  local mid = '│' .. string.rep(' ', opts.width - margin) .. '│'
+  local bot = '╰' .. string.rep('─', opts.width - margin) .. '╯'
+  table.insert(border, top);
+  for i=1, opts.height - margin do
+    table.insert(border, mid);
+  end
+  table.insert(border, bot);
+  api.nvim_buf_set_lines(self.border_bufnr, 0, -1, true, border)
+  return vim.tbl_extend('keep', opts, options)
 end
 
-function M.open()
-  local win_height = api.nvim_get_option('lines')
-  local win_width = api.nvim_get_option('columns')
+function Window:open()
+  local term_height = api.nvim_get_option('lines')
+  local term_width = api.nvim_get_option('columns')
   local opts = {
     relative = 'editor',
-    focusable = true,
-    width =  80,
+    width = 80,
     height = 20,
-    col = win_width - 81,
-    row = win_height - 23,
+    col = term_width - 81,
+    row = term_height - 23,
     anchor = 'NW',
     style = 'minimal',
   }
-  local win = api.nvim_open_win(M.bufnr, false, opts)
-  -- api.nvim_win_set_option(win, 'winhl', 'Normal:NormalFloat')
-  api.nvim_win_set_option(win, 'winblend', 30)
-  return win
+  local border_options = self:create_border(opts)
+  self.winnr = api.nvim_open_win(self.bufnr, false, opts)
+  self.border_winnr = api.nvim_open_win(self.border_bufnr, false, border_options)
+  api.nvim_win_set_option(self.border_winnr, 'winhl', 'Normal:Floating')
+  api.nvim_win_set_option(self.border_winnr, 'winblend', 30)
+  api.nvim_win_set_option(self.winnr, 'winhl', 'Normal:Floating')
+  api.nvim_win_set_option(self.winnr, 'winblend', 30)
+end
+
+function Window:clear()
+  if self:is_valid() then
+    api.nvim_buf_set_lines(self.bufnr, 0, -1, true, {})
+  end
+end
+
+function Window:close()
+  api.nvim_win_close(self.winnr, true)
+  api.nvim_win_close(self.border_winnr, true)
+end
+
+function Window:destroy()
+  api.nvim_buf_delete(self.bufnr, { force = true })
+  api.nvim_buf_delete(self.border_bufnr, { force = true })
+end
+
+function M.clear()
+  M.window:clear()
 end
 
 function M.toggle()
-  if M.is_open() then
-    api.nvim_win_close(M.winnr, true)
+  if M.window:is_open() then
+    M.window:close()
   else
-    M.winnr = M.open()
+    M.window:open()
   end
 end
 
@@ -59,27 +122,19 @@ function M.set_mappings()
 end
 
 sclang.on_start = function()
-  M.bufnr = api.nvim_create_buf(true, true)
-  api.nvim_buf_set_option(M.bufnr, 'filetype', 'scnvim')
-  api.nvim_buf_set_name(M.bufnr, '[sclang]')
-  M.winnr = M.open()
+  M.window = Window:new()
+  M.window:open()
 end
 
 sclang.on_read = function(data)
-  if M.is_valid() then
-    api.nvim_buf_set_lines(M.bufnr, -1, -1, true, {data})
-    if M.is_open() then
-      local num_lines = api.nvim_buf_line_count(M.bufnr)
-      api.nvim_win_set_cursor(M.winnr, {num_lines, 0})
-    end
-  end
+  M.window:set_lines(data)
 end
 
 sclang.on_exit = function(code, signal)
-  if M.is_open() then
-    api.nvim_win_close(M.winnr, true)
+  if M.window:is_open() then
+    M.window:close()
   end
-  api.nvim_buf_delete(M.bufnr, { force = true })
+  M.window:destroy()
 end
 
 return M
