b0bf09e7af98be1f0e889b9a98d911fcd348327e
David Granström <david@davidgranstrom.com>
2021-09-03 11:47:29 +0200

    Update plugins

--- lua/dkg/plugins.lua
+++ lua/dkg/plugins.lua
@@ -1,4 +1,5 @@
 return require'packer'.startup(function()
+  use 'wbthomason/packer.nvim'
   use 'tmsvg/pear-tree'
   use 'tpope/vim-commentary'
   use 'tpope/vim-surround'
@@ -14,7 +15,7 @@ return require'packer'.startup(function()
   }
   use {
     'nvim-treesitter/playground',
-    cmd = 'TSPlaygroundToggle'
+    cmd = ':TSPlaygroundToggle'
   }
   use 'danymat/neogen'
   -- use 'gpanders/nvim-parinfer'
@@ -40,15 +41,20 @@ return require'packer'.startup(function()
 
   -- language
   use '~/code/vim/scnvim'
-  use 'ziglang/zig.vim'
+  use {
+    'ziglang/zig.vim',
+    ft = 'zig',
+  }
   use {
     'Olical/conjure',
-    tag = 'v4.23.0'
+    tag = 'v4.23.0',
+    ft = 'fennel',
   }
-  use {
-    'Olical/aniseed',
-    tag = 'v3.21.0',
-  }
+  -- use {
+  --   'Olical/aniseed',
+  --   tag = 'v3.21.0',
+  -- }
+  use 'bakpakin/fennel.vim'
 
   -- color schemes / appearance
   use 'folke/tokyonight.nvim'
@@ -56,10 +62,16 @@ return require'packer'.startup(function()
   use 'kyazdani42/nvim-web-devicons'
 
   -- misc
-  use 'alec-gibson/nvim-tetris'
+  use {
+    'alec-gibson/nvim-tetris',
+    cmd = ':Tetris'
+  }
   use 'editorconfig/editorconfig-vim'
   use 'tpope/vim-repeat'
   use 'tpope/vim-unimpaired'
-  use '~/code/vim/nvim-markdown-preview'
+  use {
+    '~/code/vim/nvim-markdown-preview',
+    cmd = ':MarkdownPreview',
+  }
   use '~/code/vim/osc.nvim'
 end)
