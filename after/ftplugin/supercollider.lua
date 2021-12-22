f46a874fe992c29cba6f698ad38281e11b2ebf86
David Granström <david@davidgranstrom.com>
2021-12-22 16:18:10 +0100

    Add a mapping for scdoc

--- after/ftplugin/supercollider.lua
+++ after/ftplugin/supercollider.lua
@@ -1,5 +1,8 @@
 local setlocal = vim.opt_local
+local buf_map = require'dkg.utils'.buf_map
 
 -- Indentation
 setlocal.tabstop = 4
 setlocal.shiftwidth = 4
+
+buf_map(0, 'n', '<leader>d', '<cmd>Telescope scdoc<cr>', {silent = true})
