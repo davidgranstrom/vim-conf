890fb60e07e2354ec2382b677b0c90f8c9fe49d5
David Granström <david@davidgranstrom.com>
2021-12-22 16:17:17 +0100

    c++ configuration

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,12 @@
+local setlocal = vim.opt_local
+local buf_map = require'dkg.utils'.buf_map
+
+setlocal.cinoptions = 'l1'
+setlocal.commentstring = [[// %s]]
+buf_map(0, 'n', '<leader>a', '<cmd>ClangdSwitchSourceHeader<cr>', {silent = true})
+
+-- Delete doxygen comments and insert function body.
+function remove_doxygen()
+  vim.cmd [[ g/^\s*\*\|\/\*/d ]]
+  vim.cmd [[ g/;/execute "normal! $xo{\<cr>}" ]]
+end
