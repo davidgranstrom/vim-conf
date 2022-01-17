4633ebf0e462cd78d473b0f976bb313db0f79324
David Granström <david@davidgranstrom.com>
2022-01-17 16:38:17 +0100

    Ignore "text" filetypes

--- lua/dkg/configs/indent-blankline.lua
+++ lua/dkg/configs/indent-blankline.lua
@@ -10,9 +10,10 @@ require'indent_blankline'.setup{
     'git',
     'markdown',
     'fennel',
+    'text',
     '',
   },
   show_first_indent_level = false,
-  show_current_context = true,
+  -- show_current_context = true,
   -- show_current_context_start = false,
 }
